fd5b14342716b9682ec71828fc6403de9d0ee671
abbasi-naim <abbasi.naim@gmail.com>
2022-01-02 23:41:54 +0300

    update badge state and increment by one

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,7 +10,7 @@ class Counter extends Component {
   };
 
   handleIncrement = () => {
-    console.log("Increment clicked!");
+    this.setState({ count: this.state.count + 1 });
   };
 
   renderTags() {
