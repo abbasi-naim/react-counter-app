042369178ae1a662b751c3335442887996c24ed4
abbasi-naim <abbasi.naim@gmail.com>
2022-01-02 20:52:48 +0300

    create an Increment button in counter class

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,12 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   render() {
-    return <React.Fragment></React.Fragment>;
+    return (
+      <React.Fragment>
+        <h1>Hello World!</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
   }
 }
 
