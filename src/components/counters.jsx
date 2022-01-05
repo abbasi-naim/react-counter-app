4242ef0a5121c46d2d28e51c2f3eec55ceba7c39
abbasi-naim <abbasi.naim@gmail.com>
2022-01-05 22:08:41 +0300

    use map method to callback the function

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,15 +3,19 @@ import Counter from "./counter";
 
 class Counters extends Component {
   state = {
-    counters: [],
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
   };
   render() {
     return (
       <div>
-        <Counter />
-        <Counter />
-        <Counter />
-        <Counter />
+        {this.state.counters.map((counter) => (
+          <Counter />
+        ))}
       </div>
     );
   }
