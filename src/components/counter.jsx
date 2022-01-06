dc087bf1f8435680cc4715aa4c47323ebe7d5922
abbasi-naim <abbasi.naim@gmail.com>
2022-01-06 22:32:00 +0300

    changed this.props.value to this.props.counter.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 class Counter extends Component {
   state = {
-    count: this.props.value,
+    count: this.props.counter.value,
     tags: ["tag1", "tag2", "tag3"],
   };
 
@@ -30,7 +30,7 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <h1>Counter #{this.props.id}</h1>
+        <h1>Counter #{this.props.counter.id}</h1>
         <Badge bg={this.getBadgeClasses()}>{this.formatCount()}</Badge>
 
         <Button
@@ -50,7 +50,7 @@ class Counter extends Component {
           Decrement
         </Button>
         <button
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
