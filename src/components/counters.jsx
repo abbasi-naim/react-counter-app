c768b0c0898530cf0b9eec649b363398f5ea3a2d
abbasi-naim <abbasi.naim@gmail.com>
2022-01-07 21:00:46 +0300

    set the index of counters to fun parameter

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -14,6 +14,7 @@ class Counters extends Component {
   handleIncrement = (counter) => {
     const counters = [...this.state.counters];
     const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
   };
 
   handleDecrement = (counter) => {
