4326ed699b1c17f90b9f377e7602bf08a6622402
abbasi-naim <abbasi.naim@gmail.com>
2022-01-08 20:56:10 +0300

    added app component to index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import NavBar from "./components/navbar";
-import React from "react";
+import React, { Component } from "react";
 import Counters from "./components/counters";
 
 class App extends Component {
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class NavBar extends Component {
-
-    render() {
-        return (
-          <nav class="navbar navbar-light bg-light">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#">
-                Navbar
-              </a>
-            </div>
-          </nav>
-        );
-    }
+  render() {
+    return (
+      <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand">Navbar</a>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default NavBar ;
+export default NavBar;
