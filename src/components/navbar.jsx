0d125bbce9e98d4362025e2f5d5d51ceb03b056d
abbasi-naim <abbasi.naim@gmail.com>
2022-01-08 20:35:57 +0300

    add navbar from bootstrap

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,15 @@ import React, { Component } from 'react';
 class NavBar extends Component {
 
     render() {
-        return (  );
+        return (
+          <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">
+                Navbar
+              </a>
+            </div>
+          </nav>
+        );
     }
 }
 
