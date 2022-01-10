3356416bd4a21a7fbb9c25f058ade0246f320070
abbasi-naim <abbasi.naim@gmail.com>
2022-01-10 22:20:11 +0300

    add navbar badge and set total value of counters

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,14 +5,12 @@ class NavBar extends Component {
   render() {
     return (
       <nav class="navbar navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand">
-            Navbar
-            <span className="badge badge-pill badge-primary">
-              {this.props.totalCounters}
-            </span>
-          </a>
-        </div>
+        <a class="navbar-brand">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {this.props.totalCounters}
+          </span>
+        </a>
       </nav>
     );
   }
