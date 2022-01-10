923420032f4cf6ed4dedc8b01bede4d8ef7200e8
abbasi-naim <abbasi.naim@gmail.com>
2022-01-10 22:02:22 +0300

    change events on counters.jsx

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,12 +2,11 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  
   render() {
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset
@@ -15,9 +14,9 @@ class Counters extends Component {
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
+            onDecrement={this.props.onDecrement}
             counter={counter}
           />
         ))}
